de52a6d1acc3d1d289bb06a030d49caf89f91efe
Vikas Kaushik <Vikas-Kaushik@users.noreply.github.com>
2016-11-12 18:40:57 +0530

    Create Graph_stl.cpp

---
+++ Graph_stl.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class Node {
+public:
+	char key;
+	int weight;
+	Node(char key = '\0', int weight = 0) : key(key), weight(weight) {};
+};
+
+class Graph {
+public:
+	unsigned int size;
+	vector<list<Node>> adList;
+	map<char, int> key_index;
+	
+	Graph(vector<char> &v) {
+		this->size = v.size();
+		for (int i = 0; i < size; ++i) {
+			adList[i].insert(adList[i].begin(), Node(v[i]));
+			key_index[v[i]] = i;
+		}
+	}
+
+	void addEdge(char src, char dest, int weight = 0) {
+		int i = key_index[src];
+		adList[i].insert(adList[i].begin(), Node(dest, weight));
+	}
+
+	void printAdList() {
+		cout << "Printing adjecency list..." << endl;
+		for (auto & it : adList) {
+			cout << "head";
+			for (auto & it1 : it) {
+				cout << " -> " << it1.key << "(" << it1.weight << ")";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	cout << "Inside main..." << endl;
+	vector<char> vertices = { 'a', 'b', 'c', 'd', 'e' };
+	Graph *g = new Graph(vertices);
+
+	g->addEdge('a', 'b');
+	g->addEdge('a', 'e', 2);
+	g->addEdge('b', 'e', 3);
+	g->addEdge('b', 'c', 4);
+	g->addEdge('b', 'd', 2);
+	g->addEdge('d', 'c', 3);
+	g->addEdge('d', 'e', 5);
+
+	g->printAdList();
+
+	//g->BFS_Traversal();
+	//g->DFS_Traversal();
+
+	getchar();
+	return 0;
+}
